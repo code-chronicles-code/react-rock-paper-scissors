64469aa8ffe0d1b9c40eb4fcd963d0dac30aa46c
Miorel-Lucian Palii <mlpalii@gmail.com>
2025-03-23 17:06:43 -0700

    Pick a random computer move and display it

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+type Move = "rock" | "paper" | "scissors";
+const MOVES: readonly Move[] = ["rock", "paper", "scissors"];
+
+function getRandomMove(): Move {
+  const index = Math.floor(Math.random() * 3);
+  return MOVES[index];
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [computerMove, setComputerMove] = React.useState<Move>(getRandomMove());
+
+  return <div>Computer move: {computerMove}</div>;
 }
 
 export default App;
