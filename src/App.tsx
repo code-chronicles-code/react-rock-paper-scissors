d581919f677f9bcfb6ce5d4503101cac52cfec72
Miorel-Lucian Palii <mlpalii@gmail.com>
2025-03-23 17:31:12 -0700

    Display the game winner

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,16 @@ function getRandomMove(): Move {
   return MOVES[index];
 }
 
+function getWinner(moveA: Move, moveB: Move): "a" | "b" | "tie" {
+  if (moveA === moveB) {
+    return "tie";
+  }
+
+  const indexA = MOVES.indexOf(moveA);
+  const indexB = MOVES.indexOf(moveB);
+  return (indexA + 1) % MOVES.length === indexB ? "b" : "a";
+}
+
 function App() {
   const [computerMove, setComputerMove] = React.useState<Move>(getRandomMove());
   const [playerMove, setPlayerMove] = React.useState<Move | null>(null);
@@ -36,10 +46,18 @@ function App() {
     setComputerMove(getRandomMove());
   };
 
+  const winnerText = {
+    a: "You win!",
+    b: "Computer wins.",
+    tie: "It's a tie...",
+  }[getWinner(playerMove, computerMove)];
+
   return (
     <div style={containerStyle}>
-      <span>Computer move: {computerMove}</span>
-      <span>Player move: {playerMove}</span>
+      <span>
+        You picked <strong>{playerMove}</strong>, computer picked{" "}
+        <strong>{computerMove}</strong>. {winnerText}
+      </span>
       <button onClick={resetGame}>Play again</button>
     </div>
   );
